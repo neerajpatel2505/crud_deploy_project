906f8f47cb79452ac2394c0710b95e2125fec7a0
Neeraj Kumar <nkurmbanshi@yahoo.com>
2024-03-19 16:55:36 +0530

    new data added

--- project/settings.py
+++ project/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'project.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 		'NAME': 'djangodb',
-# 		'USER': 'root',
-# 		'PASSWORD' : 'root',
-# 		'HOST': 'localhost',
-# 		'PORT': '5432', # default PostgreSQL port
-# 		}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'djangodb',
+		'USER': 'root',
+		'PASSWORD' : 'root',
+		'HOST': 'localhost',
+		'PORT': '5432', # default PostgreSQL port
+		}
+}
 
 
 # Password validation
